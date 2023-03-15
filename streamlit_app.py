b6ac3557378a0ce30a137f26ff1f0c26cd5c666e
acsmith333 <127986545+acsmith333@users.noreply.github.com>
2023-03-15 13:37:27 -0700

    Update streamlit_app.py
    Added Breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Blueberry Smoothie')
+streamlit.text('Mushroom Omelet')
+streamlit.text('Chocolate Chip Pancakes')
